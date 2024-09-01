1651d345f72704e6fba00751c3206fe2b40b9400
Malini Singh <21JE0526@iitism.ac.in>
2024-09-02 02:35:27 +0530

    path to server.js update in run-script

--- app/api/run-script/route.js
+++ app/api/run-script/route.js
@@ -7,7 +7,7 @@ const execPromise = util.promisify(exec);
 export async function POST(request) {
   try {
     // const response = await execPromise('node ../internshala-backend/server.js');
-    const response = await execPromise('node ../internshala-frontend/server.js');
+    const response = await execPromise('node server.js');
 
     console.log(response.stdout);
     return NextResponse.json({ success: true, message: 'Script executed successfully' });
